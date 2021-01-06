390d8f53a1ab9d237b4d0d6c9afb9734757a998c
TeriMHopkins <TeriMHopkins@users.noreply.github.com>
2021-01-06 05:16:31 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace JobApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            String UserAnswer1 = Console.ReadLine();
+            int Age = Convert.ToInt32(UserAnswer1);
+
+            Console.WriteLine("Have you ever had a DUI? true or false");
+            String UserAnswer2 = Console.ReadLine();
+            bool Dui = Convert.ToBoolean(UserAnswer2);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            String UserAnswer3 = Console.ReadLine();
+            int SpeedTicket = Convert.ToInt32(UserAnswer3);
+
+            Console.WriteLine("Qualified?");
+            bool isQualified = (Age >= 16 && Dui == false && SpeedTicket <= 3 );
+            Console.WriteLine(isQualified);
+            Console.ReadLine();
+        }
+    }
+}
